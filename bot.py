719b9ce09887b47070a4f494c014b406af65aac4
Samantha <samantha.goldstein31@gmail.com>
2015-07-24 18:32:30 -0400

    halfway there

--- bot.py
+++ bot.py
@@ -1,12 +1,8 @@
 from twython import Twython
 import sys
-#import json
-#from datetime import datetime, timedelta
 import random
-from markov_aparrish import *
-#from markov_aparrish import char_level_generate
-#from markov_aparrish import word_level_generate
 import os
+from pseudo_markov import *
 
 def main():
     #api_key, api_secret, access_token, token_secret = sys.argv[1:]
@@ -17,16 +13,19 @@ def main():
 
 def generateTweet():
 
-    fl = open('data/bspears_sample.txt', 'r')
+    # fl = open('data/bspears_sample.txt', 'r')
+
+    fl = open('data/small_sample.txt', 'r')
     lines = fl.readlines()
-    gen = word_level_generate(lines, 2, 10);
-    # model = build_model(lines,2)
-    #gen = generate(model, 2)
-    # gen = generate_from_token_lists(model, 2)
 
+    ###TODO: string parsing here!!!!!
 
+    
+    # print lines[0:4];
+    # print " ".join(lines)
+    gen  = generateModel(" ".join(lines[:len(lines)/2]), " ".join(lines[len(lines)/2:-1]), 2)
 
-    return gen #'hello world'
+    return gen
 
 if __name__ == '__main__':
     print generateTweet()
--- pseudo_markov.py
+++ pseudo_markov.py
@@ -4,6 +4,45 @@ This function takes in text1, text2, and n
     text2: corrolary text to append to end of t1 phrases
     n: the size of the tokens generated
 """
-def generateModel(T1, T2, n):
-        
-    return 0
+
+
+from random import *
+
+def generatePhrase(t1, n):
+    # print T1[:5], "more: ", len(T2)
+    ls1 = t1.split()
+
+    # Find a location that is between zero and
+    # n places from the end of the list
+    location = randrange(0, len(ls1)-n)
+
+    phrase = ' '.join(ls1[location:location+n])
+    return phrase
+
+
+def findPhrase(startWord, corpus, n):
+    st = corpus.lower() #.split()
+
+    #TODO: don't just use the fist one you see! Set up a more glamorous
+    # algo that randomly chooses an instance of this word
+    location = st.find(startWord.lower())
+
+    if (location>=0 and location+ 50 <= len(st)  ):
+        return st[location: location+50]
+
+    else:
+        return "BAH HUMBUG"
+
+
+
+if __name__ == '__main__':
+    b_fl = open('data/bspears_sample.txt','r')
+    brit = b_fl.read()
+    s_fl = open('data/shakes_sample.txt', 'r').read()
+    n=8
+    phrase = generatePhrase(brit, n)
+    print "FIRST PHRASE: " ,phrase
+    print "SECOND PHRASE: ", findPhrase(phrase.split()[-1].lower(), s_fl, n)
+
+
+    # main()
