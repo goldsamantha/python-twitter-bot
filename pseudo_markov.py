6bfeba2f396a25330b46732033195b4877ecdff4
Samantha <samantha.goldstein31@gmail.com>
2015-07-26 14:00:00 +0100

    broad strokes

--- pseudo_markov.py
+++ pseudo_markov.py
@@ -23,17 +23,31 @@ def generatePhrase(t1, n):
 def findPhrase(st1, corpus, n):
     st = corpus.lower() #.split()
 
-    
+    searchPhraseLs = st1.split()
+    if (len(searchPhraseLs)<=n):
+        searchPhrase = st1.lower()
+    else:
+        #TODO: check that this passes at correct index
+        searchPhrase = " ".join(searchPhraseLs[len(searchPhraseLs)-n:]).lower()
+
+    print "Search phrase is: " , searchPhrase
+
+
+
 
     #TODO: don't just use the fist one you see! Set up a more glamorous
     # algo that randomly chooses an instance of this word
-    location = st.find(st1.lower())
+    location = st.find(searchPhrase)
 
     if (location>=0 and location+ 50 <= len(st)  ):
         return st[location: location+50]
 
+    elif (len(searchPhraseLs)>1):
+        print "is empty?: ", searchPhraseLs #(searchPhraseLs[0] == '')
+        return findPhrase(" ".join(searchPhraseLs[len(searchPhraseLs)-n:]), corpus,n-1)
+
     else:
-        return "BAH HUMBUG"
+        return -1
 
 
 
@@ -44,7 +58,9 @@ if __name__ == '__main__':
     n=8
     phrase = generatePhrase(brit, n)
     print "FIRST PHRASE: " ,phrase
-    print "SECOND PHRASE: ", findPhrase(phrase.split()[-1].lower(), s_fl, n)
+    # print "SECOND PHRASE: ", findPhrase(phrase.split()[-1].lower(), s_fl, 3)
+    val = findPhrase(phrase, s_fl, 3)
+    print val
 
 
     # main()
